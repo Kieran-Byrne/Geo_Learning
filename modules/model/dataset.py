b2a57ef33c442656552a9397a7ccd58417a1dd8a
Kenjiro Luong <173304175+LN244@users.noreply.github.com>
2024-08-26 14:42:44 +0200

    Update dataset.py

--- modules/model/dataset.py
+++ modules/model/dataset.py
@@ -3,7 +3,7 @@ import shutil
 import numpy as np
 import pandas as pd
 
-from ml_logic.params import *
+from modules.model.params import *
 
 from tensorflow.keras.utils import image_dataset_from_directory
 
